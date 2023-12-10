3a2930660a8f4d3cb428014043da7bc26add6297
Andy Chen <acchen@cpp.edu>
2023-12-09 17:28:04 -0800

    create key pair and store into respective directories

---
+++ generate_key_pair.py
@@ -0,0 +1,20 @@
+from Crypto.PublicKey import RSA
+import os
+
+KEY_SIZE = 2048
+
+def generate_keys(party_name):
+   key_pair = RSA.generate(KEY_SIZE)
+
+   private_file = './{0}_key/{0}_private_key.pem'.format(party_name)
+   os.makedirs(os.path.dirname(private_file), exist_ok=True)
+   with open(private_file, 'wb') as file:
+      file.write( key_pair.export_key('PEM') )
+
+   public_file = './public_keys/{0}_public_key.pem'.format(party_name)
+   os.makedirs(os.path.dirname(public_file), exist_ok=True)
+   with open(public_file, 'wb') as file:
+      file.write( key_pair.publickey().export_key('PEM') )
+
+generate_keys('sender')
+generate_keys('reciever')
